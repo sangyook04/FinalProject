686f2b9b9745ae3c9c98cbf746e532f0dee50e29
sjisoo1020 <sjisoo1020@gmail.com>
2019-07-30 14:06:22 +0900

    leader, payment,refund service

--- src/main/java/com/scorpion/service/RefundServiceImple.java
+++ src/main/java/com/scorpion/service/RefundServiceImple.java
@@ -1,5 +1,56 @@
 package com.scorpion.service;
 
+import java.util.List;
+
+import com.scorpion.domain.Criteria;
+import com.scorpion.domain.RefundVO;
+import com.scorpion.mapper.RefundMapper;
+
 public class RefundServiceImple implements RefundService {
+
+	
+	private RefundMapper mapper;
+	
+	@Override
+	public int getTotal(Criteria cri) {
+		
+		return 0;
+	}
+
+	@Override
+	public List<RefundVO> getList(Criteria cri) {
+		
+		return null;
+	}
+
+	@Override
+	public List<RefundVO> getList() {
+		
+		return null;
+	}
+
+	@Override
+	public void register(RefundVO refund) {
+		
+		
+	}
+
+	@Override
+	public RefundVO get(String leaderid) {
+		
+		return null;
+	}
+
+	@Override
+	public boolean modify(RefundVO refund) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean remove(String leaderid) {
+		
+		return false;
+	}
 
 }
