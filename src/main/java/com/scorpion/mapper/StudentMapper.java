45a8c437e3f5806b53fe459f20a327e1330842c8
sangyook04 <sangyook04@gmail.com>
2019-07-30 11:50:12 +0900

    통합

--- src/main/java/com/scorpion/mapper/StudentMapper.java
+++ src/main/java/com/scorpion/mapper/StudentMapper.java
@@ -1,13 +1,16 @@
 package com.scorpion.mapper;
 
+import java.util.List;
+
 import com.scorpion.domain.Criteria;
 import com.scorpion.domain.StudentVO;
 
 public interface StudentMapper {
-	public int update(StudentVO student);
-	public int delete(String stuid);
-	public StudentVO read(String stuid);
-	public Integer insertSelectKey(StudentVO student);
-	public int getTotalCount(Criteria cri);
+   public int update(StudentVO student);
+   public int delete(String stuid);
+   public StudentVO read(String stuid);
+   public Integer insertSelectKey(StudentVO student);
+   public int getTotalCount(Criteria cri);
+   public List<StudentVO> getListWithPaging(Criteria cri);
+    
 }
-
