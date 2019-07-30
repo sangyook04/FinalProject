d1a756e0e0cbd4cf788c8a8281e3c024a0aa4b8d
tlsxovud1 <tlsxovud1@gmail.com>
2019-07-30 14:07:51 +0900

    서비스겸 맵퍼

--- src/main/java/com/scorpion/service/QnaService.java
+++ src/main/java/com/scorpion/service/QnaService.java
@@ -1,5 +1,26 @@
 package com.scorpion.service;
 
+import java.util.List;
+
+import com.scorpion.domain.Criteria;
+import com.scorpion.domain.QnaPageDTO;
+import com.scorpion.domain.QnaReplyVO;
+import com.scorpion.domain.QnaVO;
+
+
+
 public interface QnaService {
+	
+	public QnaPageDTO getListPage(Criteria cri, Long qno);
+	public List<QnaVO> getList(Criteria cri, Long qno); 
+	public int modify(QnaVO reply);
+	public int remove(Long qno);
+	public QnaVO get(Long qno);
+	public int register(QnaVO qnavo);
+	
+	public int replymodify(QnaReplyVO reply);
+	public int replyremove(Long qno);
+	public QnaReplyVO replyget(Long qno);
+	public int replyregister(QnaReplyVO qnavo);
 
 }
--- src/main/java/com/scorpion/service/QnaServiceImple.java
+++ src/main/java/com/scorpion/service/QnaServiceImple.java
@@ -1,5 +1,72 @@
 package com.scorpion.service;
 
+import java.util.List;
+
+import com.scorpion.domain.Criteria;
+import com.scorpion.domain.QnaPageDTO;
+import com.scorpion.domain.QnaReplyVO;
+import com.scorpion.domain.QnaVO;
+
 public class QnaServiceImple implements QnaService {
+
+	@Override
+	public QnaPageDTO getListPage(Criteria cri, Long qno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<QnaVO> getList(Criteria cri, Long qno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int modify(QnaVO reply) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int remove(Long qno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public QnaVO get(Long qno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int register(QnaVO qnavo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int replymodify(QnaReplyVO reply) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int replyremove(Long qno) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public QnaReplyVO replyget(Long qno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int replyregister(QnaReplyVO qnavo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
